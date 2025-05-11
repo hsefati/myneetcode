60eb5474f7cbc3c87c879dfc99120eee4556d2b5
Hamed Sefati <hsefati@gmail.com>
2025-05-11 14:54:50 +0200

    feat: add group anagrams

--- arrays_and_hashing/group_anagram.py
+++ arrays_and_hashing/group_anagram.py
@@ -1,12 +1,25 @@
 from typing import List
+from collections import Counter
 
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        None
+        list_anagrams = []
+        checked_strs = []
+        for i in range(len(strs)):
+            if strs[i] not in checked_strs:
+                anagrams = [strs[i]]
+                for j in range(i + 1, len(strs)):
+                    if Counter(strs[i]) == Counter(strs[j]):
+                        anagrams.append(strs[j])
+                        checked_strs.append(strs[j])
+                        checked_strs.append(strs[i])
+                if anagrams:
+                    list_anagrams.append(anagrams)
 
+        return list_anagrams
 
 
 if __name__ == "__main__":
     test = Solution()
-    print(test.groupAnagrams(strs = ["act","pots","tops","cat","stop","hat"]))
+    print(test.groupAnagrams(strs=["", ""]))
