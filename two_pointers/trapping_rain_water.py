ef3b26cc63d950e38a7dfdd7264b811756cb00eb
Hamed Sefati <hsefati@gmail.com>
2025-07-22 20:07:00 +0200

    feat: add trapping rain water

---
+++ two_pointers/trapping_rain_water.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
+        res = 0
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+        return res
+
+
+if __name__ == "__main__":
+    test = Solution()
+    print(test.trap(height=[0, 2, 0, 3, 1, 0, 1, 3, 2, 1]))
